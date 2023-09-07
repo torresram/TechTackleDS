96d868c45dc80f3d730f71129953af0cb1b5482d
torresram <ramirotorres90@yahoo.com.ar>
2023-09-07 02:03:41 -0300

    Clases

---
+++ dominio/DiccionarioTablas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+
+    //Contenedor de las tablas que se necesitan en el programa actualmente
+    public static class DiccionarioTablas
+    {
+        public static Dictionary<int, string> tablaInsumos = new Dictionary<int, string>()
+        {
+            {0,"Anilla" },
+            {1,"Anzuelo" },
+            {2,"Blister" },
+            {3,"Carcasa" },
+            {4,"Carton" },
+            {5,"Piton" }
+        };
+    }
+}
