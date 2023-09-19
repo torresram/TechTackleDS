835bc9a9bfac2815d3057fe91dc454ca84943b94
torresram <ramirotorres90@yahoo.com.ar>
2023-09-19 00:26:40 -0300

    Formularios y reportes

---
+++ TechSeñuelos/frmListaRemitos.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace TechSeñuelos
+{
+    partial class frmListaRemitos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvListaRemitos = new System.Windows.Forms.DataGridView();
+            this.dgvDetalleRemito = new System.Windows.Forms.DataGridView();
+            this.lblBuscador = new System.Windows.Forms.Label();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListaRemitos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetalleRemito)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvListaRemitos
+            // 
+            this.dgvListaRemitos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dgvListaRemitos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListaRemitos.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvListaRemitos.Location = new System.Drawing.Point(18, 84);
+            this.dgvListaRemitos.MultiSelect = false;
+            this.dgvListaRemitos.Name = "dgvListaRemitos";
+            this.dgvListaRemitos.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dgvListaRemitos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvListaRemitos.Size = new System.Drawing.Size(397, 537);
+            this.dgvListaRemitos.TabIndex = 0;
+            this.dgvListaRemitos.SelectionChanged += new System.EventHandler(this.dgvListaRemitos_SelectionChanged);
+            // 
+            // dgvDetalleRemito
+            // 
+            this.dgvDetalleRemito.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDetalleRemito.Location = new System.Drawing.Point(457, 84);
+            this.dgvDetalleRemito.MultiSelect = false;
+            this.dgvDetalleRemito.Name = "dgvDetalleRemito";
+            this.dgvDetalleRemito.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dgvDetalleRemito.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvDetalleRemito.Size = new System.Drawing.Size(428, 536);
+            this.dgvDetalleRemito.TabIndex = 1;
+            // 
+            // lblBuscador
+            // 
+            this.lblBuscador.AutoSize = true;
+            this.lblBuscador.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBuscador.Location = new System.Drawing.Point(15, 35);
+            this.lblBuscador.Name = "lblBuscador";
+            this.lblBuscador.Size = new System.Drawing.Size(73, 16);
+            this.lblBuscador.TabIndex = 2;
+            this.lblBuscador.Text = "BUSCAR:";
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(94, 31);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(321, 20);
+            this.txtBuscar.TabIndex = 3;
+            this.txtBuscar.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtBuscar.TextChanged += new System.EventHandler(this.txtBuscar_TextChanged);
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnImprimir.Location = new System.Drawing.Point(918, 84);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(123, 53);
+            this.btnImprimir.TabIndex = 4;
+            this.btnImprimir.Text = "&Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            // 
+            // frmListaRemitos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1053, 663);
+            this.Controls.Add(this.btnImprimir);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.lblBuscador);
+            this.Controls.Add(this.dgvDetalleRemito);
+            this.Controls.Add(this.dgvListaRemitos);
+            this.Name = "frmListaRemitos";
+            this.Text = "LISTADO REMITOS";
+            this.Load += new System.EventHandler(this.frmListaRemitos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListaRemitos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetalleRemito)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvListaRemitos;
+        private System.Windows.Forms.DataGridView dgvDetalleRemito;
+        private System.Windows.Forms.Label lblBuscador;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Button btnImprimir;
+    }
+}
---
+++ TechSeñuelos/frmListaRemitos.cs
@@ -0,0 +1,111 @@
+﻿using dominio;
+using negocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TechSeñuelos
+{
+    public partial class frmListaRemitos : Form
+    {
+        List<Remito> remitos = new List<Remito>();
+        List<Armado> armado = new List<Armado>();
+        private Remito seleccionado;
+
+        public frmListaRemitos()
+        {
+            InitializeComponent();
+        }
+        private void frmListaRemitos_Load(object sender, EventArgs e)
+        {
+            RemitoNeg remitoNeg = new RemitoNeg();
+            remitos = remitoNeg.listar();
+            dgvListaRemitos.DataSource = remitos;
+
+            formatoDgvs();
+        }
+        private void dgvListaRemitos_SelectionChanged(object sender, EventArgs e)
+        {
+            if (seleccionado == null)
+            {
+                seleccionado = (Remito)dgvListaRemitos.SelectedRows[0].DataBoundItem;
+                obtenerRemito(seleccionado);
+                dgvDetalleRemito.DataSource = armado;
+                dgvDetalleRemito.Refresh();
+            }
+            else
+            {
+                seleccionado = (Remito)dgvListaRemitos.CurrentRow.DataBoundItem;
+                obtenerRemito(seleccionado);
+                dgvDetalleRemito.DataSource = armado;
+                dgvDetalleRemito.Refresh();
+            }
+        }
+        private void txtBuscar_TextChanged(object sender, EventArgs e)
+        {
+            List<Remito> listaFiltrada;
+
+            string filtro = txtBuscar.Text;
+
+            if(filtro.Length >= 1)
+            {
+                listaFiltrada = remitos.FindAll(x => x.Destino.ToLower().Contains(filtro.ToLower()) || x.Numero.ToString().ToLower().Contains(filtro.ToLower()));
+            }
+            else
+            {
+                listaFiltrada = remitos;
+            }
+
+            //dgvListaRemitos.DataSource = null;
+            dgvListaRemitos.DataSource = listaFiltrada;
+            if(listaFiltrada != null)
+            {
+                seleccionado = (Remito)dgvListaRemitos.SelectedRows[0].DataBoundItem;
+                formatoDgvs();
+            }
+            else
+            {
+                dgvListaRemitos.DataSource = remitos;
+            }
+        }
+
+
+        private void formatoDgvs()
+        {
+            dgvListaRemitos.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvListaRemitos.Columns["Id"].Visible = false;
+            dgvListaRemitos.Columns["Fecha"].DefaultCellStyle.Format = "dd/MM/yyyy";
+            dgvListaRemitos.Columns["Fecha"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dgvListaRemitos.Columns["Fecha"].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgvListaRemitos.Columns["Numero"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dgvListaRemitos.Columns["Numero"].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgvListaRemitos.Columns["Destino"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dgvListaRemitos.Columns["Destino"].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            dgvDetalleRemito.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvDetalleRemito.Columns["Id"].Visible = false;
+            dgvDetalleRemito.Columns["Remito"].Visible = false;
+            dgvDetalleRemito.Columns["Artificial"].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgvDetalleRemito.Columns["Artificial"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgvDetalleRemito.Columns["Color"].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgvDetalleRemito.Columns["Color"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgvDetalleRemito.Columns["Cantidad"].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgvDetalleRemito.Columns["Cantidad"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+        }
+        private void obtenerRemito(Remito remito)
+        {
+            ArmadoNeg armadoNeg = new ArmadoNeg();
+            int nroRemito = remito.Numero;
+
+            armado = armadoNeg.detalleRemito(nroRemito);
+        }
+
+    }
+}
