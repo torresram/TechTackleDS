3c10b66091eb5177c718d8a8ff61bd65d58d8265
torresram <ramirotorres90@yahoo.com.ar>
2024-06-25 00:16:51 -0300

    Consultas SQL

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -1,4 +1,3 @@
---select * from Color
 --select distinct Color,Id from Color order by Color asc
 
 --select distinct Destino from Remito order by Destino asc
@@ -10,12 +9,9 @@
 --select Id,Numero,Destino from Remito where Numero = 1120
 --select Id,Id_artificial,Id_color, Id_remito, Cantidad from Armado where Id_remito = 11
 
-
 --insert into Armado (Id_artificial, Id_color, Id_remito, Cantidad) values (5,5,1,85)
 
---select A.Id,R.Numero Remito, S.Modelo, C.Color,A.Cantidad from Armado A,Color C,Artificial S,Remito R
---where Id_remito = R.Id and A.Id_artificial = S.Id and A.Id_color = C.Id and R.Numero = 4
-
+--SELECT A.Id Id, R.Numero, S.Modelo, C.Color, A.Cantidad FROM Armado A, Color C, Artificial S,Remito R WHERE Id_artificial = S.Id AND A.Id_color = C.Id AND Id_remito = R.Id
 
 --update Armado set Id_artificial = 6, Id_color = 5, Cantidad = 35 where Id = 4
 
@@ -34,36 +30,43 @@
 
 --SELECT COUNT(*) AS Total FROM Armado WHERE Id_artificial = 6 AND Id_color = 11 and Id_remito = 5
 
---select * from Armado 
---select * from Remito
---select * from Anilla
---select * from Anzuelo
---select * from Blister
---select * from Carcasa
---select * from Carton
---select * from Piton
---select * from Artificial
---select * from Standar
+--SELECT S.Id, S.Modelo, A.Tamaño AnillaA,A.Id AnAnzId,Pit.Tamaño AnillaP, Pit.Id AnPitId, H.Numero AnzSimple,H.Id ASId, HT.Numero AnzTriple, HT.Id ATId, B.Modelo Blister, B.Id idBlister, C.Modelo Carton, C.Id idCarton,P.modelo Piton, P.id idPiton 
+--FROM Anilla A, Anilla Pit, Anzuelo H, Anzuelo HT, Standar S, Blister B, Carton C, PITON P 
+--WHERE S.IdAnzueloS = H.Id AND S.IdAnzueloT = HT.Id AND S.IdAnillasA = A.Id AND S.IdAnillasP = Pit.Id AND S.IdCarton = C.Id AND S.IdBlister = B.Id AND S.idPiton = P.id
 
+select A.Id,R.Numero Remito, S.Modelo, C.Color,A.Cantidad from Armado A,Color C,Artificial S,Remito R
+where Id_remito = R.Id and A.Id_artificial = S.Id and A.Id_color = C.Id and R.Numero = 14
+
+select * from Color
+select * from Anilla
+select * from Anzuelo
+select * from Blister
+select * from Carcasa
+select * from Carton
+select * from Piton
+select * from ARTIFICIAL
+select * from ARMADO
+select * from Remito
+select * from Standar
+
+--SELECT DISTINCT id, modelo from CARTON ORDER BY modelo 
 --UPDATE Armado SET Cantidad = @Cantidad WHERE Id_artificial = @Art AND Id_color = @Color AND Id_remito = @Remito
 --UPDATE Armado SET Cantidad = @Cantidad WHERE Id = @Id
-
---truncate table standar
+--UPDATE ANZUELO SET  descripcion = 'NO LLEVA' where id = 4
+--UPDATE STANDAR SET idAnillasA = @aniAnzuelo, idAnillasP = @aniPaleta, idAnzueloS = @anzSimple, idAnzueloT = @anzTriple, idBlister = @blister, idCarton = @carton, idPiton = @piton WHERE id = @id
+ 
+--truncate table armado; truncate table remito
 
 --select Cantidad from Armado where Id_artificial = 1 and Id_color = 1 and Id_remito = 14
 
 --delete from Anzuelo where Id = 1007
-
+ 
 --select Id from Remito where Numero = 3
 
 --insert into Standar (Modelo, IdAnillasA, IdAnillasP, IdAnzueloS, IdAnzueloT, IdBlister, IdCarton, IdPiton) values ('Arrow',2,4,2,1,1,1,1)
 --update Standar SET IdAnillasP = 1005, IdBlister = 1, IdCarton = 8 WHERE Id = 2
 --update Standar SET IdBlister = 4, IdCarton = 10 WHERE Id = 1004
 --select * from Standar
-
---SELECT S.Id, S.Modelo, A.Tamaño AnillaA,Pit.Tamaño AnillaP, H.Numero AnzSimple, HT.Numero AnzTriple, C.Modelo Carton, B.Modelo Blister, P.Modelo Piton
---FROM Anilla A, Anilla Pit, Anzuelo H, Anzuelo HT, Carton C, Blister B, Standar S, Piton P 
---WHERE S.IdAnzueloS = H.Id AND S.IdAnzueloT = HT.Id AND S.IdAnillasA = A.Id AND S.IdAnillasP = Pit.Id AND S.IdCarton = C.Id AND S.IdBlister = B.Id AND S.IdPiton = P.Id
 
 --select distinct Id, Numero from Anzuelo order by Numero asc
 
@@ -72,11 +75,11 @@
 --insert into Anilla(Tamaño) values (0)
 --insert into Anzuelo(Numero) values (0)
 
---select S.Id, S.Modelo, A.Tamaño AnillaA,A.Id AnAnzId,Pit.Tamaño AnillaP, Pit.Id AnPitId, H.Numero AnzSimple,H.Id ASId, HT.Numero AnzTriple, HT.Id ATId, B.Modelo Blister, B.Id idBlister, C.Modelo Carton, C.Id idCarton
---from Anilla A, Anilla Pit, Anzuelo H, Anzuelo HT, Standar S, Blister B, Carton C
---where S.IdAnzueloS = H.Id and S.IdAnzueloT = HT.Id and S.IdAnillasA = A.Id and S.IdAnillasP = Pit.Id and S.IdCarton = C.Id and S.IdBlister = B.Id and S.Modelo = 'Bendy 1'
+--SELECT S.Id, S.Modelo, A.Tamaño AnillaA,A.Id AnAnzId,Pit.Tamaño AnillaP, Pit.Id AnPitId, H.Numero AnzSimple,H.Id ASId, HT.Numero AnzTriple, HT.Id ATId, B.Modelo Blister, B.Id idBlister, C.Modelo Carton, C.Id idCarton,P.modelo Piton, P.id idPiton
+--FROM Anilla A, Anilla Pit, Anzuelo H, Anzuelo HT, Standar S, Blister B, Carton C, PITON P
+--WHERE S.IdAnzueloS = H.Id AND S.IdAnzueloT = HT.Id AND S.IdAnillasA = A.Id AND S.IdAnillasP = Pit.Id AND S.IdCarton = C.Id AND S.IdBlister = B.Id AND S.idPiton = P.id
 
---delete from Standar where Id = 5
+--delete from Standar where Id = 11
 
 --update Standar set IdAnillasA = 3, IdAnillasP = 2, IdAnzueloS = 7, IdAnzueloT = 2 where Id = 2
 --update Anzuelo set Descripcion = 'No lleva', Cantidad = 0, Peso = 0 where Id = 7
@@ -118,4 +121,19 @@
 
 --UPDATE ARTIFICIAL SET codigo = '', id_color = 1, modelo = '', imagen = '', cantidad = 1, peso = 1.2 WHERE id = 1
 
-SELECT Codigo, Modelo, C.Color Color, Imagen, Peso, Cantidad, C.Id IdColor, C.Descripcion Descripcion FROM Artificial A, Color C WHERE A.Id_color = C.Id
+--SELECT Codigo, Modelo, C.Color Color, Imagen, Peso, Cantidad, C.Id IdColor, C.Descripcion Descripcion FROM Artificial A, Color C WHERE A.Id_color = C.Id
+
+--SELECT S.id, S.modelo Modelo, A.tamaño AnillaA,Pit.tamaño AnillaP, H.numero AnzSimple, HT.numero AnzTriple, C.modelo Carton, B.modelo Blister, P.modelo Piton
+--FROM ANILLA A, ANILLA Pit, ANZUELO H, ANZUELO HT, CARTON C, BLISTER B, STANDAR S, PITON P
+--WHERE S.idAnzueloS = H.id AND S.idAnzueloT = HT.id AND S.idAnillasA = A.id AND S.idAnillasP = Pit.id AND S.idCarton = C.id AND S.idBlister = B.id AND S.idPiton = P.id
+
+--SELECT A.Id, Codigo, Modelo, C.Color Color, Imagen, Peso, Cantidad, C.Id IdColor, C.Descripcion Descripcion 
+--FROM Artificial A, Color C 
+--WHERE A.Id_color = C.Id 
+--ORDER BY modelo ASC
+
+--SELECT DISTINCT modelo FROM ARTIFICIAL
+
+--insert into STANDAR (modelo,idAnillasA,idAnillasP,idAnzueloS,idAnzueloT, idBlister,idCarton,idPiton) values ('@model',0,0,0,0,0,0,0)
+
+--SELECT A.Id, Codigo, Modelo, C.Color Color, Imagen, Peso, Cantidad, C.Id IdColor, C.Descripcion Descripcion FROM Artificial A, Color C WHERE A.Id_color = C.Id ORDER BY modelo ASC
